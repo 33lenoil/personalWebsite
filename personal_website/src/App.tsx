3ba97edfd98aeb81d6949a9c0fc628359401cc18
lionelhupenn <143546299+lionelhupenn@users.noreply.github.com>
2024-03-17 22:03:36 -0400

    iniital index page setup

--- personal_website/src/App.tsx
+++ personal_website/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Routes,
+    Route,
 } from 'react-router-dom';
 
+import Index from './pages/index';
+
 export default function App() {
-    
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Index />} />
+            </Routes>
+        </Router>
+    )
 }
